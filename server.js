465a8cdfff29c9ab1c6a8daedd1131b97b35554c
Nidhi Mehta <nidhi.mehta.ntf@gmail.com>
2026-04-22 16:16:04 +0530

    emailadd

--- server.js
+++ server.js
@@ -168,4 +168,5 @@ app.post("/webhook", async (req, res) => {
 ================================= */
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
+  sendKlaviyoEvent("dwarkeshsaxena.ntf@gmal.com");
 });
